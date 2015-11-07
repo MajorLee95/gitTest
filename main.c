cda298381b6f1fcde2cdb3959a8ed3955f5a0c1a
MajorLee <joel@vorobotics.com>
2015-11-07 16:20:22 +0100

    init serial and wire

--- main.c
+++ main.c
@@ -8,6 +8,12 @@ void IIChandlerRec(int);
 void requestEvent(void);
 
 void setup(){
+
+	Serial.begin(115200);
+	Serial.println("IIC Slave");
+	Wire.begin(0x05);
+	Wire.onReceive(IIChandlerRec);
+	Wire.onRequest(requestEvent);
 	pinMode(13, OuTPUT);
 	pinMode(10, INPUT);
 }
